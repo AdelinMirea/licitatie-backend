f9ff0af3253deb870c860a6de696d9bbc9277256
Adelin Mirea <adelinmirea@gmail.com>
2019-10-16 17:48:58 +0300

    Initial commit

---
+++ src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/ServerLicitatieApplication.java
@@ -0,0 +1,13 @@
+package com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerLicitatieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerLicitatieApplication.class, args);
+	}
+
+}
