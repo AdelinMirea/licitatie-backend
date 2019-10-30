4dc9d7e189ef141c3388c9015fcd87d24a64cdaf
Mirea Radu <radumirea99@gmail.com>
2019-10-30 14:48:59 +0200

    removed useless annotations for user

--- src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/domain/User.java
+++ src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/domain/User.java
@@ -16,53 +16,25 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private Integer id;
-    @Getter
-    @Setter
     private String firstName;
-    @Getter
-    @Setter
     private String lastName;
     @Column(unique = true)
-    @Getter
-    @Setter
     private String mail;
-    @Getter
-    @Setter
     private String password;
-    @Getter
-    @Setter
     private String userToken;
-    @Getter
-    @Setter
     private Boolean verified;
-    @Getter
-    @Setter
     private Date lastActive;
-    @Getter
-    @Setter
     private Double rating;
-    @Getter
-    @Setter
     private Integer numberOfRatings;
-    @Getter
-    @Setter
     private Double numberOfCredits;
     @ToString.Exclude
     @OneToMany
-    @Getter
-    @Setter
     private List<Bid> bids;
     @ToString.Exclude
     @OneToMany
-    @Getter
-    @Setter
     private List<Auction> auctions;
     @ToString.Exclude
     @OneToMany
-    @Getter
-    @Setter
     private List<Comment> comments;
 }
