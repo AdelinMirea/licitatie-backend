8d93297500ba8239c09d0e66442f0a0ea8a658a0
Mirea Radu <radumirea99@gmail.com>
2019-10-30 14:51:17 +0200

    removed annotations for userdto

--- src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/dto/UserDTO.java
+++ src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/dto/UserDTO.java
@@ -6,41 +6,19 @@ import lombok.Setter;
 import java.sql.Date;
 import java.util.List;
 
+@Getter
+@Setter
 public class UserDTO {
-    @Getter
-    @Setter
     private Integer id;
-    @Getter
-    @Setter
     private String firstName;
-    @Getter
-    @Setter
     private String lastName;
-    @Getter
-    @Setter
     private String mail;
-    @Getter
-    @Setter
     private Boolean verified;
-    @Getter
-    @Setter
     private Date lastActive;
-    @Getter
-    @Setter
     private Double rating;
-    @Getter
-    @Setter
     private Integer numberOfRatings;
-    @Getter
-    @Setter
     private Double numberOfCredits;
-    @Getter
-    @Setter
     private List<Integer> bidsIds;
-    @Getter
-    @Setter
     private List<Integer> auctionsIds;
-    @Getter
-    @Setter
     private List<Integer> commentsIds;
 }
