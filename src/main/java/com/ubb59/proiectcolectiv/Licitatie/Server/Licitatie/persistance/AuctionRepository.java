1e96a3f128b797847a4d7b291a0ed6f69ee3a8d9
Mirea Adelin <adelinmirea@gmail.com>
2019-11-04 22:34:47 +0200

    Add endpoint to get auctions Remaining tests to do.

--- src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/persistance/AuctionRepository.java
+++ src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/persistance/AuctionRepository.java
@@ -1,12 +1,16 @@
 package com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.persistance;
 
 import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.domain.Auction;
+import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.domain.Category;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface AuctionRepository extends JpaRepository<Auction, Integer> {
+    List<Auction> findAllByTitleContainingOrDescriptionContainingOrCategoryIn(String title, String descriprion, List<Category> category, Pageable of);
 }
--- src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/persistance/CategoryRepository.java
+++ src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/persistance/CategoryRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findAllByNameContaining(String filter);
 }
