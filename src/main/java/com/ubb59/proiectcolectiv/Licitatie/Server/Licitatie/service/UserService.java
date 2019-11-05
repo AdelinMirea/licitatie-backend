43a27a0279f1d3d20e51b7749e341af3c786c96b
Mirea Radu <radumirea99@gmail.com>
2019-11-05 21:46:08 +0200

    added user update feature

--- src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/service/UserService.java
+++ src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/service/UserService.java
@@ -4,6 +4,7 @@ import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.domain.User;
 import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.persistance.UserRepository;
 import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.validator.AuthenticationException;
 import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.validator.DataValidationException;
+import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.validator.TokenException;
 import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.validator.Validator;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class UserService {
@@ -35,14 +37,46 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public User updateUser(String token, String firstName, String lastName, String email) throws TokenException, DataValidationException {
+        Optional<User> userOptional = userRepository.findByUserToken(token);
+        if(!userOptional.isPresent()){
+            throw new TokenException("Invalid token");
+        }else {
+            User updatedUser = userOptional.get();
+            updatedUser.setFirstName(firstName);
+            updatedUser.setLastName(lastName);
+            updatedUser.setMail(email);
+            validator.validateUser(updatedUser);
+            userRepository.save(updatedUser);
+            return updatedUser;
+        }
+    }
+
+    public void updateUserPassword(String token, String oldPassword, String newPassword) throws TokenException, DataValidationException {
+        Optional<User> userOptional = userRepository.findByUserToken(token);
+        if(!userOptional.isPresent()){
+            throw new TokenException("Invalid token");
+        }else {
+            User updatedUser = userOptional.get();
+            String hashedPasswordOldPassword = DigestUtils.sha256Hex(oldPassword);
+            if(!updatedUser.getPassword().equals(hashedPasswordOldPassword)){
+                throw new DataValidationException("Invalid old password");
+            }
+            updatedUser.setPassword(newPassword);
+            validator.validateUser(updatedUser);
+            updatedUser.setPassword(DigestUtils.sha256Hex(newPassword));
+            userRepository.save(updatedUser);
+        }
+    }
+
     public String getUserTokenByCredentials(String email, String password) throws AuthenticationException {
         String hashedPassword = DigestUtils.sha256Hex(password);
         List<User> users = userRepository.findAllByMailEquals(email);
-        if(users.isEmpty()){
+        if (users.isEmpty()) {
             throw new AuthenticationException("No user with such email exists");
         }
         User user = users.get(0);
-        if(!user.getPassword().equals(hashedPassword)){
+        if (!user.getPassword().equals(hashedPassword)) {
             throw new AuthenticationException("Wrong password");
         }
         return user.getUserToken();
---
+++ src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/validator/TokenException.java
@@ -0,0 +1,7 @@
+package com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.validator;
+
+public class TokenException extends Exception{
+    public TokenException(String message) {
+        super(message);
+    }
+}
