8021a20c224d82945f3297b0a2d6843ee7dc5abb
Mirea Adelin <adelinmirea@gmail.com>
2019-11-06 22:45:33 +0200

    Update test import. Update test import that causes error while running.

--- src/test/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/service/UserServiceTests.java
+++ src/test/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/service/UserServiceTests.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import sun.tools.jstat.Token;
 
 import javax.persistence.EntityExistsException;
 import java.sql.Date;
