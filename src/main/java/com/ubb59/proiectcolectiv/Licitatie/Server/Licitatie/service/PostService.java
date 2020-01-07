09fb4b36a5e1eb561f20934bc5b347024a44ee7b
Mirea Radu <radumirea99@gmail.com>
2020-01-07 08:18:26 +0200

    (53) get pentru postdto (#39)
    * added rest GET for Category
    
    * added REST GET endpoint for Post by Auction Id
    
    * changed REST GET mapping

--- src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/service/PostService.java
+++ src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/service/PostService.java
@@ -2,14 +2,17 @@ package com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.service;
 
 import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.domain.Comment;
 import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.domain.Post;
+import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.dto.PostDTO;
 import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.persistance.CommentRepository;
 import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.persistance.PostRepository;
+import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.util.DTOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @Transactional
@@ -17,11 +20,13 @@ public class PostService {
 
     private PostRepository postRepository;
     private CommentRepository commentRepository;
+    private DTOUtils dtoUtils;
 
     @Autowired
-    PostService(PostRepository postRepository, CommentRepository commentRepository) {
+    PostService(PostRepository postRepository, CommentRepository commentRepository, DTOUtils dtoUtils) {
         this.postRepository = postRepository;
         this.commentRepository = commentRepository;
+        this.dtoUtils = dtoUtils;
     }
 
     /*
@@ -38,5 +43,14 @@ public class PostService {
         comments.add(comment);
         post.setComments(comments);
         return postRepository.save(post);
+    }
+
+    public PostDTO getPostByAuctionId(Integer auctionId){
+        Optional<Post> postOptional = postRepository.findByAuction_Id(auctionId);
+        if(postOptional.isPresent()){
+            return dtoUtils.postToPostDTO(postOptional.get());
+        }else {
+            throw new EntityNotFoundException("No auction with such id exists");
+        }
     }
 }
