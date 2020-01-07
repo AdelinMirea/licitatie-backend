addea3904d65fdcbb9ec79fe34c20f3354b83016
Mirea Radu <radumirea99@gmail.com>
2020-01-07 22:32:42 +0200

    added REST GET endpoint for a post's comments

--- src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/persistance/CommentRepository.java
+++ src/main/java/com/ubb59/proiectcolectiv/Licitatie/Server/Licitatie/persistance/CommentRepository.java
@@ -1,12 +1,15 @@
 package com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.persistance;
 
 import com.ubb59.proiectcolectiv.Licitatie.Server.Licitatie.domain.Comment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findByPost_IdOrderByDatePostedDesc(Integer postId, Pageable of);
 }
